fb29254f16085a41a3f6ee139c63dd116497c6ea
九月三秋 <993623938@qq.com>
2019-06-15 11:49:30 +0800

    初次提交

---
+++ App.vue
@@ -0,0 +1,22 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "common/main.css";
+	@import "common/icon.css";
+	@import "common/weui.wxss";
+	@import "common/bootstrap.wxss";
+	@import "common/vant.css";
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="text-center text-df border radius">
+		<view class="grid col-5 py-2">
+			<view>代码</view>
+			<view>名称</view>
+			<view>最新价</view>
+			<view>估值</view>
+			<view>是否低估</view>
+		</view>
+		<view  class="grid col-5 py-1" v-for="(item,index) in tableList" :key="index">
+			<view>
+				000001
+			</view>
+			<view>
+				上证指数
+			</view>
+			<view>
+				2852.13
+			</view>
+			<view>
+				2500
+			</view>
+			<view>
+				是
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tableList:[1,2,3,4,5,6,7,8,9,10]
+			}
+		},
+	}
+</script>
+
+<style>
+</style>
