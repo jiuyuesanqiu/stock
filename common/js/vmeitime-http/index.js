dcc47da0546c6e2684db10baacad52f4f8645cad
九月三秋 <993623938@qq.com>
2019-06-30 17:54:46 +0800

    优化cookie获取，首页样式优化

--- common/js/vmeitime-http/index.js
+++ common/js/vmeitime-http/index.js
@@ -5,6 +5,7 @@ http.interceptor.response = (response) => {
 	return response.data.data;
 }
 
+
 /**
  * 获取cookie
  */
@@ -17,10 +18,30 @@ uni.request({
 	}
 })
 
+//等待函数
+function timeout(ms) {
+	return new Promise((resolve, reject) => {
+		setTimeout(resolve, ms, 'done');
+	});
+}
+
+const getCookie = async () => {
+	while (true) {
+		if (cookie != '') {
+			return;
+		} else {
+			await timeout(50);
+		}
+	}
+}
+
+
 /**
  *获取单只股票利润表
  */
 const income = async (stockCode) => {
+	await getCookie();
+
 	return http.request({
 		url: '/v5/stock/finance/cn/income.json?type=S0&is_detail=true&count=8',
 		method: 'GET',
@@ -37,6 +58,8 @@ const income = async (stockCode) => {
  * 获取单只股票报价
  */
 const quote = async (stockCode) => {
+	await getCookie();
+
 	return http.get('/v5/stock/quote.json?extend=detail', {
 		symbol: stockCode
 	}, {
@@ -50,6 +73,7 @@ const quote = async (stockCode) => {
  * 分页获取所有股票
  */
 const quoteList = async (page = 1, size = 10, order = 'asc', orderby = 'code') => {
+	await getCookie();
 	return http.get('https://xueqiu.com/service/v5/stock/screener/quote/list?order_by=symbol&type=sh_sz', {
 		page,
 		size,
@@ -66,8 +90,10 @@ const quoteList = async (page = 1, size = 10, order = 'asc', orderby = 'code') =
  * 搜索股票
  * 
  */
-const search = (code)=>{
-	return http.get(`https://xueqiu.com/stock/search.json?code=${code}&size=50&page=1`,{},{
+const search = async (code) => {
+	await getCookie();
+
+	return http.get(`https://xueqiu.com/stock/search.json?code=${code}&size=50&page=1`, {}, {
 		header: {
 			'cookie': cookie
 		}
@@ -77,19 +103,22 @@ const search = (code)=>{
 /**
  * 获取K线图
  */
-const keyline = (stockCode)=>{
-	let begin = new Date().getTime()- 6 * 365 * 24 * 60 * 60 *1000;
+const keyline = async (stockCode) => {
+	await getCookie();
+
+	let begin = new Date().getTime() - 6 * 365 * 24 * 60 * 60 * 1000;
 	let end = new Date().getTime();
-	return http.get(`/v5/stock/chart/kline.json?symbol=${stockCode}&begin=${begin}&end=${end}&period=day&type=before&indicator=kline`,{},{
-		header: {
-			'cookie': cookie
-		}
-	})
+	return http.get(
+		`/v5/stock/chart/kline.json?symbol=${stockCode}&begin=${begin}&end=${end}&period=day&type=before&indicator=kline`, {}, {
+			header: {
+				'cookie': cookie
+			}
+		})
 }
 // 默认全部导出  import api from '@/common/vmeitime-http/'
 export default {
 	income, //获取单只股票利润表
 	quote, //获取单只股票报价
 	quoteList, //获取股票列表
-	search,//搜索股票
+	search, //搜索股票
 }
